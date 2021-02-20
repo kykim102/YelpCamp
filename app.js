dbc9bf3d7c098ffd152af56ee011bcf19a98ba40
kykim102 <58721219+kykim102@users.noreply.github.com>
2021-02-20 16:04:12 +1300

    Authentication setup using passport

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
 const campgroundsRoute = require('./routes/campgrounds');
 const reviewsRoute = require('./routes/reviews');
@@ -46,10 +49,23 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
   res.locals.success = req.flash('success');
   res.locals.error = req.flash('error');
   next();
+});
+
+app.get('/fakeUser', async (req, res) => {
+  const user = new User({ email: 'kyu@gamil.com', username: 'kyu' });
+  const newUser = await User.register(user, 'chicken');
+  res.send(newUser);
 });
 
 app.get('/', (req, res) => {
